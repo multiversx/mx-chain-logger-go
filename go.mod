a91911573d849d200457827fe1bc430c55e5c763
Iulian Pascalau <iulian.pascalau@gmail.com>
2020-03-25 17:49:16 +0200

    new logger implementation

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/ElrondNetwork/elrond-go-logger
+
+go 1.13
+
+require (
+	github.com/gogo/protobuf v1.3.1
+	github.com/golang/protobuf v1.3.5
+	github.com/stretchr/testify v1.5.1
+)
---
+++ mock/formatterStub.go
@@ -0,0 +1,18 @@
+package mock
+
+import "github.com/ElrondNetwork/elrond-go-logger"
+
+// FormatterStub -
+type FormatterStub struct {
+	OutputCalled func(line logger.LogLineHandler) []byte
+}
+
+// Output -
+func (fs *FormatterStub) Output(line logger.LogLineHandler) []byte {
+	return fs.OutputCalled(line)
+}
+
+// IsInterfaceNil -
+func (fs *FormatterStub) IsInterfaceNil() bool {
+	return fs == nil
+}
