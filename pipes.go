442c8aed04924129e022c82723aa982607153675
Andrei Bancioiu <andrei.bancioiu@elrond.com>
2020-04-07 21:18:41 +0300

    Continue implementation of piped logging.

--- pipes.go
+++ pipes.go
@@ -1,24 +1,135 @@
 package logger
 
 import (
+	"encoding/binary"
 	"io"
 	"os"
+	"time"
 )
 
 var _ io.Writer = (*pipeObserver)(nil)
+
+const sizeOfUint32 = 4
 
 type pipeObserver struct {
 	writePipe *os.File
 }
 
-// NewPipeObserver -
+// NewPipeObserver creates a new observer that can be attached to any logger,
+// and which writes the log data through a pipe.
+// Ultimately, the data will be read by a "pipeObserverForwarder"
 func NewPipeObserver(writePipe *os.File) *pipeObserver {
 	return &pipeObserver{
 		writePipe: writePipe,
 	}
 }
 
-// Write -
-func (observer *pipeObserver) Write(p []byte) (n int, err error) {
-	return observer.writePipe.Write(p)
+// Write sends a marshalized log line through the pipe, to be captured by the forwarder
+// We have to ensure this is thread-safe
+func (observer *pipeObserver) Write(logLineMarshalized []byte) (int, error) {
+	length := len(logLineMarshalized)
+	err := observer.writeLogLineLength(length)
+	if err != nil {
+		return 0, err
+	}
+
+	return observer.writePipe.Write(logLineMarshalized)
+}
+
+func (observer *pipeObserver) writeLogLineLength(length int) error {
+	buffer := make([]byte, sizeOfUint32)
+	binary.LittleEndian.PutUint32(buffer, uint32(length))
+	_, err := observer.writePipe.Write(buffer)
+	return err
+}
+
+type pipeObserverForwarder struct {
+	readPipe    *os.File
+	marshalizer Marshalizer
+	loggerSink  *logger
+}
+
+// NewPipeObserverForwarder creates a new forwarder that reads log lines from a pipe
+// and sends them to a generic logger sink
+func NewPipeObserverForwarder(readPipe *os.File, marshalizer Marshalizer, loggerSink *logger) *pipeObserverForwarder {
+	return &pipeObserverForwarder{
+		readPipe:    readPipe,
+		marshalizer: marshalizer,
+		loggerSink:  loggerSink,
+	}
+}
+
+func (forwarder *pipeObserverForwarder) StartFowarding() {
+	go forwarder.continuouslyReadLogLines()
+}
+
+func (forwarder *pipeObserverForwarder) continuouslyReadLogLines() {
+	for {
+		logLine, err := forwarder.readLogLine()
+		if err != nil {
+			forwarder.loggerSink.Error("continuouslyReadLogLines error", "err", err)
+			break
+		}
+
+		forwarder.loggerSink.Log(logLine)
+	}
+}
+
+func (forwarder *pipeObserverForwarder) readLogLine() (*LogLine, error) {
+	length, err := forwarder.readLogLineLength()
+	if err != nil {
+		return nil, err
+	}
+
+	logLineWrapper, err := forwarder.readLogLinePayload(length)
+	if err != nil {
+		return nil, err
+	}
+
+	logLine := forwarder.recoverLogLine(logLineWrapper)
+	return logLine, nil
+}
+
+func (forwarder *pipeObserverForwarder) readLogLineLength() (uint32, error) {
+	buffer := make([]byte, sizeOfUint32)
+	_, err := io.ReadFull(forwarder.readPipe, buffer)
+	if err != nil {
+		return 0, err
+	}
+
+	length := binary.LittleEndian.Uint32(buffer)
+	return length, nil
+}
+
+func (forwarder *pipeObserverForwarder) readLogLinePayload(length uint32) (*LogLineWrapper, error) {
+	buffer := make([]byte, length)
+	_, err := io.ReadFull(forwarder.readPipe, buffer)
+	if err != nil {
+		return nil, err
+	}
+
+	logLine := &LogLineWrapper{}
+	err = forwarder.marshalizer.Unmarshal(logLine, buffer)
+	if err != nil {
+		return nil, err
+	}
+
+	return logLine, nil
+}
+
+func (forwarder *pipeObserverForwarder) recoverLogLine(wrapper *LogLineWrapper) *LogLine {
+	logLine := &LogLine{
+		LoggerName:  wrapper.LoggerName,
+		Correlation: wrapper.Correlation,
+		Message:     wrapper.Message,
+		LogLevel:    LogLevel(wrapper.LogLevel),
+		Args:        make([]interface{}, len(wrapper.Args)),
+		Timestamp:   time.Unix(0, wrapper.Timestamp),
+	}
+
+	for i, str := range wrapper.Args {
+		logLine.Args[i] = str
+	}
+
+	return logLine
 }
--- pipes_test.go
+++ pipes_test.go
@@ -1,17 +1,58 @@
 package logger
 
 import (
+	"encoding/json"
 	"os"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 )
 
 func Test_ChildToParentThroughPipes(t *testing.T) {
+	ToggleLoggerName(true)
+
 	// Parent sets up the pipes
 	readLogsFromChildFile, writeLogsToParentFile, err := os.Pipe()
 	require.Nil(t, err)
+	require.NotNil(t, readLogsFromChildFile)
+	require.NotNil(t, writeLogsToParentFile)
+
+	// Parent setup
+	parentOutputSubject := NewLogOutputSubject()
+	parentOutputSubject.ClearObservers()
+	parentOutputSubject.AddObserver(os.Stdout, &ConsoleFormatter{})
+	genericLoggerSink := GetOrCreate("generic")
+	parentForwarder := NewPipeObserverForwarder(readLogsFromChildFile, &jsonMarshalizer{}, genericLoggerSink)
+	parentForwarder.StartFowarding()
 
 	// Child setup
 	pipeObserver := NewPipeObserver(writeLogsToParentFile)
+	childOutputSubject := NewLogOutputSubject()
+	childOutputSubject.ClearObservers()
+	logLineFormatter, _ := NewLogLineWrapperFormatter(&jsonMarshalizer{})
+	childOutputSubject.AddObserver(pipeObserver, logLineFormatter)
+	childLogger := newLogger("child/foo", LogTrace, childOutputSubject)
+
+	// Child writes logs
+	childLogger.Trace("test")
+	childLogger.Trace("foobar")
+	childLogger.Trace("foo", "answer", 42)
+
+	time.Sleep(1 * time.Second)
+}
+
+type jsonMarshalizer struct {
+}
+
+func (marshalizer *jsonMarshalizer) Marshal(obj interface{}) ([]byte, error) {
+	return json.Marshal(obj)
+}
+
+func (marshalizer *jsonMarshalizer) Unmarshal(obj interface{}, buff []byte) error {
+	return json.Unmarshal(buff, obj)
+}
+
+func (marshalizer *jsonMarshalizer) IsInterfaceNil() bool {
+	return marshalizer == nil
 }
