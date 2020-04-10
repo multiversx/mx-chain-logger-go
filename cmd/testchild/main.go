ad4e969d5b3bde239f5475435ff83777696653e2
Andrei Bancioiu <andrei.bancioiu@elrond.com>
2020-04-10 13:17:13 +0300

    Fix main of test.

--- cmd/testchild/main.go
+++ cmd/testchild/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	goLog "log"
 	"os"
 
 	"github.com/ElrondNetwork/elrond-go-logger/marshal"
@@ -14,36 +15,21 @@ const (
 )
 
 func main() {
-	errCode, errMessage := doMain()
-	if errCode != 0 {
-		fmt.Fprintln(os.Stderr, errMessage)
-		os.Exit(errCode)
-	}
-}
-
-// doMain returns (error code, error message)
-func doMain() (int, string) {
 	profileReader := getPipeFile(fileDescriptorProfileReader)
 	if profileReader == nil {
-		return 42, "Cannot get pipe file: [profileReader]"
+		goLog.Fatal("Cannot get pipe file: [profileReader]")
 	}
 
 	logsWriter := getPipeFile(fileDescriptorLogsWriter)
 	if logsWriter == nil {
-		return 42, "Cannot get pipe file: [logsWriter]"
+		goLog.Fatal("Cannot get pipe file: [logsWriter]")
 	}
 
-	part, err := pipes.NewChildPart(profileReader, logsWriter, &marshal.JSONMarshalizer{})
+	part := pipes.NewChildPart(profileReader, logsWriter, &marshal.JSONMarshalizer{})
+	err := part.StartLoop()
 	if err != nil {
-		return 42, fmt.Sprintf("Cannot create ChildPart: %v", err)
+		goLog.Fatal("Ended loop")
 	}
-
-	err = part.StartLoop()
-	if err != nil {
-		return 42, fmt.Sprintf("Ended loop: %v", err)
-	}
-
-	return 0, ""
 }
 
 func getPipeFile(fileDescriptor uintptr) *os.File {
