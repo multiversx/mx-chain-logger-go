3d949fb75424f662cbd78705ba6e3c4ec154acc7
Andrei Bancioiu <andrei.bancioiu@elrond.com>
2020-04-13 15:52:12 +0300

    Formatting.

--- pipes/childPart_test.go
+++ pipes/childPart_test.go
@@ -54,15 +54,16 @@ func TestChildPart_ConcurrentWriteLogs(t *testing.T) {
 	profileReader := os.NewFile(4242, "/proc/self/fd/4242")
 	logsWriter := os.NewFile(4343, "/proc/self/fd/4343")
 
-	logLineMarshalizer := &marshal.JSONMarshalizer{}
-	childLogger := logger.GetOrCreate("child-log")
-	childPart, err := NewChildPart(profileReader, logsWriter, logLineMarshalizer)
+	childPart, err := NewChildPart(profileReader, logsWriter, &marshal.JSONMarshalizer{})
 	require.Nil(t, err)
+
 	err = childPart.StartLoop()
 	require.Nil(t, err)
 
 	wg := sync.WaitGroup{}
 	wg.Add(2)
+
+	childLogger := logger.GetOrCreate("child-log")
 
 	go func() {
 		for i := 0; i < 1000; i++ {
