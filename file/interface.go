bdde7158643c8b76969227e0f71ace21d4114cc5
Iuga Mihai <mihai9625@gmail.com>
2022-09-01 14:29:03 +0300

    file logging

---
+++ file/interface.go
@@ -0,0 +1,10 @@
+package logging
+
+import "time"
+
+// FileLoggingHandler will handle log file rotation
+type FileLoggingHandler interface {
+	ChangeFileLifeSpan(newDuration time.Duration, newSizeInMB uint64) error
+	Close() error
+	IsInterfaceNil() bool
+}
